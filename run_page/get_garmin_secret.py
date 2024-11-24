759ad097368a2563903ae6cf8486c74ea4ab5059
Wei Sun <sagasw@gmail.com>
2024-11-24 02:44:18 +0000

    format py issue

--- run_page/get_garmin_secret.py
+++ run_page/get_garmin_secret.py
@@ -15,7 +15,6 @@ if __name__ == "__main__":
     options = parser.parse_args()
     if options.is_cn:
         garth.configure(domain="garmin.cn")
-    
     garth.login(options.email, options.password)
     secret_string = garth.client.dumps()
     print(secret_string)
